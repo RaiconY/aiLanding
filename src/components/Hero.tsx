c0b0e1452f2665e2deed0cb51bd50434c310e246
Ilya P <46619650+RaiconY@users.noreply.github.com>
2025-05-11 14:43:39 +0700

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero: React.FC = () => {
             <div className="grid grid-cols-3 gap-2 sm:gap-4 pt-2">
               {[
                 { number: "+50", text: "шаблонов для экономии 2+ часов в день" },
-                { number: "+20", text: "промптов заменяющих работу дизайнера" },
+                { number: "+20", text: "промптов, заменяющих работу дизайнера" },
                 { number: "+10", text: "готовых GPTs для автоматизации основных задач" }
               ].map((stat, index) => (
                 <div 
