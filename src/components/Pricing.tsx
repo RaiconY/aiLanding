635986bcede407b6a9fece204c8087b2a0433204
Ilya P <46619650+RaiconY@users.noreply.github.com>
2025-06-01 22:25:14 +0700

    Updated Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -11,28 +11,28 @@ const Pricing: React.FC = () => {
           <div className="max-w-2xl mx-auto space-y-8">
             <div className="text-center space-y-6">
               <p className="text-xl text-gray-900">
-                Стартовая цена гайда — <span className="font-semibold">2990₽</span>
+                Стартовая цена гайда — <span className="font-semibold">3&nbsp;990&nbsp;₽</span>
               </p>
               <p className="text-lg text-gray-700">
-                После добавления нового контента цена вырастет до 5990₽
+                После добавления нового контента цена вырастет до 5&nbsp;990&nbsp;₽
               </p>
               <p className="text-lg text-gray-700">
-                Если ваш час стоит 50+$, то этот гайд окупится уже через пару часов
+                Если ваш час стоит более 3&nbsp;000&nbsp;₽, то этот гайд окупится уже через пару часов
               </p>
             </div>
 
             <div className="flex flex-col items-center pt-8 border-t border-gray-100">
               <div className="flex items-center gap-6 mb-8">
                 <span className="text-3xl font-bold text-gray-400 line-through">
-                  5 990 ₽
+                  5&nbsp;990&nbsp;₽
                 </span>
                 <span className="text-4xl font-bold bg-gradient-to-r from-[#E76832] to-[#FFC24B] bg-clip-text text-transparent">
-                  2 990 ₽
+                  3&nbsp;990&nbsp;₽
                 </span>
               </div>
 
               <a 
-                href="https://t.me/Ilya_Plv" 
+                href="https://payform.ru/c97mLiR/" 
                 target="_blank"
                 rel="noopener noreferrer"
                 className="bg-gradient-to-r from-[#E76832] to-[#FFC24B] text-white font-medium px-8 py-4 rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 text-lg"
@@ -42,7 +42,13 @@ const Pricing: React.FC = () => {
               
               {/* Мелкий текст под кнопкой с увеличенной шириной */}
               <p className="mt-4 text-sm text-gray-500 max-w-xl w-full px-4 sm:px-8 text-center">
-                Кнопка ведёт в Telegram. Напишите мне, что хотите купить гайд, я вышлю ссылку на оплату и доступ после подтверждения. Пока еще не успел автоматизировать 🥲
+                После нажатия кнопки вы перейдете на защищенную страницу оплаты. И сразу после оплаты получите ссылку на гайд 🫡
+                <br /><br />По всем вопросам можете писать <a 
+                  href="https://t.me/grigorets_assistant" 
+                  className="text-[#E76832] hover:text-[#FFC24B] transition-colors duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >@grigorets_assistant</a>
               </p>
             </div>
           </div>
