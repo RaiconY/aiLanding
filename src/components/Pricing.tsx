d197108a1b5537b0004e34c36b6810688c20ec12
Ilya P <46619650+RaiconY@users.noreply.github.com>
2025-06-03 22:28:16 +0700

    Updated Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -11,10 +11,10 @@ const Pricing: React.FC = () => {
           <div className="max-w-2xl mx-auto space-y-8">
             <div className="text-center space-y-6">
               <p className="text-xl text-gray-900">
-                Стартовая цена гайда — <span className="font-semibold">3&nbsp;990&nbsp;₽</span>
+                Стартовая цена гайда — <span className="font-semibold">4&nbsp;990&nbsp;₽</span>
               </p>
               <p className="text-lg text-gray-700">
-                3 июня цена вырастет до&nbsp;7&nbsp;777&nbsp;₽
+                3 июня цена вырастет до&nbsp;6&nbsp;990&nbsp;₽
               </p>
               <p className="text-lg text-gray-700">
                 Если ваш час стоит более 3&nbsp;000&nbsp;₽, то этот гайд окупится уже через пару часов
@@ -24,15 +24,15 @@ const Pricing: React.FC = () => {
             <div className="flex flex-col items-center pt-8 border-t border-gray-100">
               <div className="flex items-center gap-6 mb-8">
                 <span className="text-3xl font-bold text-gray-400 line-through">
-                  7&nbsp;777&nbsp;₽
+                  6&nbsp;990&nbsp;₽
                 </span>
                 <span className="text-4xl font-bold bg-gradient-to-r from-[#E76832] to-[#FFC24B] bg-clip-text text-transparent">
-                  3&nbsp;990&nbsp;₽
+                  4&nbsp;990&nbsp;₽
                 </span>
               </div>
 
               <a 
-                href="https://t.me/Ilya_Plv" 
+                href="https://payform.ru/s37Ay19/" 
                 target="_blank"
                 rel="noopener noreferrer"
                 className="bg-gradient-to-r from-[#E76832] to-[#FFC24B] text-white font-medium px-8 py-4 rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 text-lg"
@@ -42,7 +42,7 @@ const Pricing: React.FC = () => {
               
               {/* Мелкий текст под кнопкой с увеличенной шириной */}
               <p className="mt-4 text-sm text-gray-500 max-w-xl w-full px-4 sm:px-8 text-center">
-                После нажатия на кнопку вас перекинет в чат со мной. Я пока еще не настроил автоматическую оплату 🫡
+                 После нажатия кнопки вы перейдете на защищенную страницу оплаты. И сразу после оплаты получите ссылку на гайд 🫡
                 <br /><br />По всем вопросам можете писать <a 
                   href="https://t.me/Ilya_Plv" 
                   className="text-[#E76832] hover:text-[#FFC24B] transition-colors duration-300"
