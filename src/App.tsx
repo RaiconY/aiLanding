2ea383ed0dae122577e360da9e29c2a97d7a5c6a
Ilya P <46619650+RaiconY@users.noreply.github.com>
2025-08-19 23:16:58 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ function App() {
   return (
     <div className="min-h-screen">
       <Hero />
-      <NewTestimonials />
       {/* <FullWidthImage imageUrl="https://egftuzzffkkyebeatghv.supabase.co/storage/v1/object/sign/images/Screens_with_circle%20(1).png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5X2QwZWQyZjQyLWU3OGEtNDk0MS05ZjEwLWMwODMwYjU1ODQ0MCJ9.eyJ1cmwiOiJpbWFnZXMvU2NyZWVuc193aXRoX2NpcmNsZSAoMSkucG5nIiwiaWF0IjoxNzQ2OTA0MzUwLCJleHAiOjE3Nzg0NDAzNTB9.fTllEdp66BfVieoSTuX2RpUKJJWI04_ar-xXQdroVhQ" /> */}
       <ValueProposition />
       {/* <Stats />  */}
@@ -36,6 +35,7 @@ function App() {
       {/* <AIDisclosure />*/}
       <FullWidthImage imageUrl="https://raw.githubusercontent.com/RaiconY/aiLanding/main/src/images/Screens with rabbit Robot.png" />
       {/*<TestimonialsScreenshots />*/}
+      <NewTestimonials />
       <Pricing />
       <FAQ />
       {/*<UnifiedAuthors />
