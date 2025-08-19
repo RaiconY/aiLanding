047b69368201c8a0730bf7c0845e1cddbac14c26
Ilya P <46619650+RaiconY@users.noreply.github.com>
2025-08-19 23:18:33 +0700

    Update Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -11,7 +11,7 @@ const Pricing: React.FC = () => {
           <div className="max-w-2xl mx-auto space-y-8">
             <div className="text-center space-y-6">
               <p className="text-xl text-gray-900">
-                Стартовая цена гайда — <span className="font-semibold">4&nbsp;990&nbsp;₽</span>
+                Сейчас гайд стоит <span className="font-semibold">4&nbsp;990&nbsp;₽</span>
               </p>
               <p className="text-lg text-gray-700">
                 Если ваш час стоит более 3&nbsp;000&nbsp;₽, то этот гайд окупится уже через пару часов
@@ -21,7 +21,7 @@ const Pricing: React.FC = () => {
             <div className="flex flex-col items-center pt-8 border-t border-gray-100">
               <div className="flex items-center gap-6 mb-8">
                 <span className="text-3xl font-bold text-gray-400 line-through">
-                  7&nbsp;777&nbsp;₽
+                  6&nbsp;990&nbsp;₽
                 </span>
                 <span className="text-4xl font-bold bg-gradient-to-r from-[#E76832] to-[#FFC24B] bg-clip-text text-transparent">
                   4&nbsp;990&nbsp;₽
